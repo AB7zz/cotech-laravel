1758d737b371e0df0819651c00a309eb6f74039f
AB7zz <abhinavcv007@gmail.com>
2024-06-11 15:57:46 +0530

    insert and display integrated

--- resources/views/coalition/index.blade.php
+++ resources/views/coalition/index.blade.php
@@ -17,13 +17,14 @@
                 <input type="text" class="form-control" id="productName" name="productName" required>
             </div>
             <div class="form-group">
-                <label for="quantityInStock">Product Name</label>
+                <label for="quantityInStock">Quantity In Stock</label>
                 <input type="number" class="form-control" id="quantityInStock" name="quantityInStock" required>
             </div>
             <div class="form-group">
-                <label for="pricePerItem">Product Name</label>
+                <label for="pricePerItem">Price Per Item</label>
                 <input type="text" class="form-control" id="pricePerItem" name="pricePerItem" required>
             </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
         </form>
         <table class="table">
             <thead>
@@ -47,5 +48,44 @@
             </tfoot>
         </div>
     </div>
+    <script>
+        $(document).ready(function() {
+            $('#productForm').on('submit', function(e) {
+                e.preventDefault();
+
+                $.ajax({
+                    type: 'POST',
+                    url: '{{ route('coalition.store') }}',
+                    data: $(this).serialize(),
+                    success: function(response) {
+                        if(response.success){
+                            let products = response.products;
+                            let sumTotalValue = response.sumTotalValue;
+
+                            $('#productTableBody').empty();
+                            products.forEach(product => {
+                                $('#productTableBody').append(`
+                                    <tr>
+                                        <td>${product.productName}</td>
+                                        <td>${product.quantityInStock}</td>
+                                        <td>${product.pricePerItem}</td>
+                                        <td>${product.dateTimeSubmitted}</td>
+                                        <td>${product.totalValueNumber}</td>
+                                        <td><a href="/products/${product.id}/edit" class="btn btn-warning btn-sm">Edit</a></td>
+                                    </tr>
+                                `)
+                            });
+
+                            $('#sumTotalValue').text(sumTotalValue);
+                            $('#productForm')[0].reset();
+                        }
+                    },
+                    error: function(response) {
+                        alert('Err');
+                    }
+                })
+            })
+        })
+    </script>
 </body>
 </html>
